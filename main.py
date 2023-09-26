f5abbcea75ba1664061c11cb83532bab42efa7e3
Jingyu Wang <jingyuwang@cnb-d102-53.inf.ethz.ch>
2023-09-26 12:42:05 +0200

    SDF and sobel validated

--- main.py
+++ main.py
@@ -1,56 +1,9 @@
 import taichi as ti
 from taichi.math import vec3, atan2, pi, acos, sin, cos
+from grid_sdf import GridSDF
+from utils import *
 
-@ti.func
-def trilerp(x, x0, x1, q000, q100, q010, q110, q001, q101, q011, q111):
-    """
-    Trilinear interpolation.
-
-    Input:
-    - x: interpolate at x
-    - x0, x1: 3D points in a right-handed system. In unit length, x0=(0,0,0), x1=(1,1,1)
-    - q000, q010, ...: values at (0,0,0), (0,1,0), ...
-
-    Output:
-    - value at x
-    """
-    fx = (x1[0] - x[0]) / (x1[0] - x0[0])
-    fy = (x1[1] - x[1]) / (x1[1] - x0[1])
-    fz = (x1[2] - x[2]) / (x1[2] - x0[2])
-    _fx = 1 - fx
-    _fy = 1 - fy
-    _fz = 1 - fz
-    return ((q000 * fx + q100 * _fx) * fy + (q010 * fx + q110 * _fx) * _fy) * fz + ((q001 * fx + q101 * _fx) * fy + (q011 * fx + q111 * _fx) * _fy) * _fz
-
-@ti.func
-def sphere_analytic_normal(o, p):
-    """
-    Analytical normal of a sphere.
-
-    args:
-    - o: vec3. Origin of the sphere.
-    - p: vec3. 3d point.
-
-    returns:
-    - n: vec3. Normal.
-    """
-    return (p-o).normalized()
-
-@ti.func
-def sphere_analytic_tangent_bitangent(o, p):
-    """
-    Analytical tangent and bitangent of a sphere.
-    """
-    n = sphere_analytic_normal(o, p)
-    theta = atan2(n.y, n.x)
-    if theta < 0:
-        theta += 2*pi
-    phi = acos(n.z)
-
-    return vec3(cos(theta)*cos(phi), cos(theta)*sin(phi), -sin(theta)), vec3(-sin(phi), cos(phi), 0)
-           
-
-ti.init()
+ti.init(arch=ti.gpu, kernel_profiler=True, device_memory_fraction=0.8)
 
 # Grid SDF config
 o = vec3(0.0)
@@ -58,6 +11,27 @@ r = 1.0
 res = 128
 lb = vec3(-1.5, -1.5, -1.5)
 rt = vec3(1.5, 1.5, 1.5)
+sdf = sd_bunny
+
+grid_sdf = GridSDF(lb, rt, res, sdf)
+grid_sdf.init_field()
+grid_sdf.calc_numeric_gradiant()
+
+# Cloud shape
+cloud = vec3.field(shape=10 * res**2)
+cloud_normal = vec3.field(shape=2*res**2)
+
+@ti.kernel
+def init_cloud():
+    for i, j, k in grid_sdf.density:
+        if ti.abs(grid_sdf.density[i, j, k]) < 1e-3:
+            n = i * res**2 + j * res + k
+            n = int(n/res)
+            p = grid_sdf.ijk2p(i, j, k)
+            cloud[n] = p
+            cloud_normal[2*n] = p
+            cloud_normal[2*n + 1] = p + 0.05 * grid_sdf.numeric_gradient[i, j, k]
+init_cloud()
 
 # Sample points to show
 n_samples = 20
@@ -87,7 +61,7 @@ def init_render_sample():
 init_render_sample()
 
 # Render config
-window = ti.ui.Window("Tangent and bitangent estimation", (1200, 1200))
+window = ti.ui.Window("Tangent and bitangent estimation", (1200, 1200), fps_limit=30)
 canvas = window.get_canvas()
 scene = ti.ui.Scene()
 camera = ti.ui.Camera()
@@ -105,19 +79,24 @@ x_axis[0], y_axis[0], z_axis[0] = vec3(0), vec3(0), vec3(0)
 x_axis[1], y_axis[1], z_axis[1] = vec3(3, 0, 0), vec3(0, 3, 0), vec3(0, 0 ,3)
 
 
+t = 0
+step = 0.01
 while window.running:
-    camera.track_user_inputs(window, movement_speed=0.03, hold_key=ti.ui.LMB)
+    camera.position(3*sin(t), 3*cos(t), 0)
     scene.set_camera(camera)
     scene.ambient_light((0.8, 0.8, 0.8))
     scene.point_light(pos=camera.curr_position, color=(1, 1, 1))
 
-    scene.particles(sphere, color = (0.5, 0.5, 0.5), radius = r)
-    scene.particles(points, color = (0.3, 0.6, 0.2), radius = r/100)
+    scene.particles(cloud, color = (0.5, 0.5, 0.5), radius = r/300)
+    scene.lines(cloud_normal, color = (0.9, 0.9, 0.1), width=3)
+    # scene.particles(points, color = (0.3, 0.6, 0.2), radius = r/100)
+    
     scene.lines(x_axis, width=3, color=(0.1, 0.1, 0.9))
     scene.lines(y_axis, width=3, color=(0.1, 0.9, 0.1))
     scene.lines(z_axis, width=3, color=(0.9, 0.1, 0.1))
-    scene.lines(normals, width=1, color=(0.9, 0.1, 0.1))
-    scene.lines(tangents, width=1, color=(0.1, 0.1, 0.9))
-    scene.lines(bitangents, width=1, color=(0.1, 0.9, 0.1))
+    # scene.lines(normals, width=1, color=(0.9, 0.1, 0.1))
+    # scene.lines(tangents, width=1, color=(0.1, 0.1, 0.9))
+    # scene.lines(bitangents, width=1, color=(0.1, 0.9, 0.1))
     canvas.scene(scene)
     window.show()
+    t += step
